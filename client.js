e2b038063b3289252f799b74e59f79b38e6487b1
Ricardo Jorge <ricardolemos18@hotmail.com>
2025-06-26 19:15:45 -0300

    Improvement on console interface, multi client msgs

--- client.js
+++ client.js
@@ -1,9 +1,54 @@
+const { rejects } = require("assert");
 const net = require("net");
+const readLine = require("readline/promises");
 
-const client = net.createConnection({ host: "127.0.0.1", port: 3080 }, () => {
-  console.log("Connected to the server!");
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-client.on("end", () => {
+const clearLine = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+const moveCursor = (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+};
+
+const socket = net.createConnection(
+  { host: "127.0.0.1", port: 3080 },
+  async () => {
+    console.log("Connected to the server!");
+
+    const ask = async () => {
+      const message = await rl.question("Enter a message > ");
+      // Move the cursor one line up
+      await moveCursor(0, -1);
+      //Clear the current line thaat the cursor is in
+      await clearLine(0);
+      socket.write(message);
+    };
+
+    ask();
+
+    socket.on("data", async (data) => {
+      //Log an empty line
+      console.log();
+      await moveCursor(0, -1);
+      await clearLine(0);
+      console.log(data.toString("utf-8"));
+      ask();
+    });
+  }
+);
+
+socket.on("end", () => {
   console.log("Connection was ended!");
 });
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const net = require("net");
 
+// TCP - A server that connects two separate endpoints to communicate.
+// IPC - communication between two process inside the same operatin system(same device)
+const server = net.createServer();
+
 const port = 3080;
 const host = "127.0.0.1";
 
-const server = net.createServer();
+// An array of client sockets
+const clients = [];
 
 server.on("connection", (socket) => {
   console.log("A new connection to the server!");
+
+  // Listener added, so everytime data comes through the stream we execute an action.
+  socket.on("data", (data) => {
+    clients.map((client) => {
+      client.write(data);
+    });
+  });
+
+  clients.push(socket);
 });
 
 server.listen(port, host, () => {
